4ec95296829f2a071fa6db164fb8f58c898eabb5
Mako Joseph Bryant <mako.j.bryant@outlook.com>
2025-01-16 09:15:28 -0800

    📁 add Level2Scene files
    and fixed format from StubScene files

---
+++ Source/Level2Scene.c
@@ -0,0 +1,119 @@
+//------------------------------------------------------------------------------
+//
+// File Name:	Level2Scene.c
+// Author(s):	Mako J. Bryant (0068410)
+// Project:		Project 1
+// Course:		CS230S25
+//
+// Copyright © 2025 DigiPen (USA) Corporation.
+//
+//------------------------------------------------------------------------------
+
+#include "stdafx.h"
+
+#include "Scene.h"
+#include "SceneSystem.h"
+#include "Level2Scene.h"
+
+//------------------------------------------------------------------------------
+// Private Constants:
+//------------------------------------------------------------------------------
+
+//------------------------------------------------------------------------------
+// Private Structures:
+//------------------------------------------------------------------------------
+
+typedef struct Level2Scene
+{
+	// WARNING: The base class must always be included first.
+	Scene	base;
+
+	// Add any scene-specific variables second.
+
+} Level2Scene;
+
+//------------------------------------------------------------------------------
+// Public Variables:
+//------------------------------------------------------------------------------
+
+//------------------------------------------------------------------------------
+// Private Variables:
+//------------------------------------------------------------------------------
+
+//------------------------------------------------------------------------------
+// Private Function Declarations:
+//------------------------------------------------------------------------------
+
+static void Level2SceneLoad(void);
+static void Level2SceneInit(void);
+static void Level2SceneUpdate(float dt);
+static void Level2SceneExit(void);
+static void Level2SceneUnload(void);
+static void Level2SceneRender(void);
+
+//------------------------------------------------------------------------------
+// Instance Variable:
+//------------------------------------------------------------------------------
+
+static Level2Scene instance =
+{
+	// Initialize the base structure:
+	{ "Stub", Level2SceneLoad, Level2SceneInit, Level2SceneUpdate, Level2SceneRender, Level2SceneExit, Level2SceneUnload },
+
+	// Initialize any scene-specific variables:
+};
+
+//------------------------------------------------------------------------------
+// Public Functions:
+//------------------------------------------------------------------------------
+
+// Get the instance of the Stub Scene.
+// Returns:
+//	 Pointer to the base Scene structure for this derived Scene.
+const Scene* Level2SceneGetInstance(void)
+{
+	return &(instance.base);
+}
+
+//------------------------------------------------------------------------------
+// Private Functions:
+//------------------------------------------------------------------------------
+
+// Load any resources used by the scene.
+static void Level2SceneLoad(void)
+{
+}
+
+// Initialize the entities and variables used by the scene.
+static void Level2SceneInit()
+{
+}
+
+// Update the the variables used by the scene.
+// Params:
+//	 dt = Change in time (in seconds) since the last game loop.
+static void Level2SceneUpdate(float dt)
+{
+	// Tell the compiler that the 'dt' variable is unused.
+	UNREFERENCED_PARAMETER(dt);
+
+	// NOTE: This call causes the engine to exit immediately.  Make sure to remove
+	//   it when you are ready to test out a new scene.
+	SceneSystemSetNext(NULL);
+}
+
+// Render any objects associated with the scene.
+void Level2SceneRender(void)
+{
+}
+
+// Free any objects associated with the scene.
+static void Level2SceneExit()
+{
+}
+
+// Unload any resources used by the scene.
+static void Level2SceneUnload(void)
+{
+}
+
