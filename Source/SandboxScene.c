ef159be6193fd81af024bea8fb1c20043940841a
Mako J. Bryant <mako.j.bryant@outlook.com>
2025-01-16 16:33:17 -0800

    📁 create SandboxScene files

---
+++ Source/SandboxScene.c
@@ -0,0 +1,119 @@
+//------------------------------------------------------------------------------
+//
+// File Name:	SandboxScene.c
+// Author(s):	Mako J. Bryant (0068410)
+// Project:		Project 1
+// Course:		CS230S25
+//
+// Copyright © 2025 DigiPen (USA) Corporation.
+//
+//------------------------------------------------------------------------------
+
+#include "stdafx.h"
+
+#include "Scene.h"
+#include "SceneSystem.h"
+#include "SandboxScene.h"
+
+//------------------------------------------------------------------------------
+// Private Constants:
+//------------------------------------------------------------------------------
+
+//------------------------------------------------------------------------------
+// Private Structures:
+//------------------------------------------------------------------------------
+
+typedef struct SandboxScene
+{
+	// WARNING: The base class must always be included first.
+	Scene	base;
+
+	// Add any scene-specific variables second.
+
+} SandboxScene;
+
+//------------------------------------------------------------------------------
+// Public Variables:
+//------------------------------------------------------------------------------
+
+//------------------------------------------------------------------------------
+// Private Variables:
+//------------------------------------------------------------------------------
+
+//------------------------------------------------------------------------------
+// Private Function Declarations:
+//------------------------------------------------------------------------------
+
+static void SandboxSceneLoad(void);
+static void SandboxSceneInit(void);
+static void SandboxSceneUpdate(float dt);
+static void SandboxSceneExit(void);
+static void SandboxSceneUnload(void);
+static void SandboxSceneRender(void);
+
+//------------------------------------------------------------------------------
+// Instance Variable:
+//------------------------------------------------------------------------------
+
+static SandboxScene instance =
+{
+	// Initialize the base structure:
+	{ "Stub", SandboxSceneLoad, SandboxSceneInit, SandboxSceneUpdate, SandboxSceneRender, SandboxSceneExit, SandboxSceneUnload },
+
+	// Initialize any scene-specific variables:
+};
+
+//------------------------------------------------------------------------------
+// Public Functions:
+//------------------------------------------------------------------------------
+
+// Get the instance of the Stub Scene.
+// Returns:
+//	 Pointer to the base Scene structure for this derived Scene.
+const Scene* SandboxSceneGetInstance(void)
+{
+	return &(instance.base);
+}
+
+//------------------------------------------------------------------------------
+// Private Functions:
+//------------------------------------------------------------------------------
+
+// Load any resources used by the scene.
+static void SandboxSceneLoad(void)
+{
+}
+
+// Initialize the entities and variables used by the scene.
+static void SandboxSceneInit()
+{
+}
+
+// Update the the variables used by the scene.
+// Params:
+//	 dt = Change in time (in seconds) since the last game loop.
+static void SandboxSceneUpdate(float dt)
+{
+	// Tell the compiler that the 'dt' variable is unused.
+	UNREFERENCED_PARAMETER(dt);
+
+	// NOTE: This call causes the engine to exit immediately.  Make sure to remove
+	//   it when you are ready to test out a new scene.
+	SceneSystemSetNext(NULL);
+}
+
+// Render any objects associated with the scene.
+void SandboxSceneRender(void)
+{
+}
+
+// Free any objects associated with the scene.
+static void SandboxSceneExit()
+{
+}
+
+// Unload any resources used by the scene.
+static void SandboxSceneUnload(void)
+{
+}
+
