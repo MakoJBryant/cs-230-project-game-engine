bcdb04a23c26617cc7a70f5b65c9f4f04b5127b6
Mako J. Bryant <mako.j.bryant@outlook.com>
2025-01-22 15:46:53 -0800

    📁 create Mesh.c file

---
+++ Source/Mesh.c
@@ -0,0 +1,109 @@
+//------------------------------------------------------------------------------
+//
+// File Name:	Mesh.c
+// Author(s):	Mako J. Bryant (0068410)
+// Project:		Project 2
+// Course:		CS230S25
+//
+// Copyright © 2025 DigiPen (USA) Corporation.
+//
+//------------------------------------------------------------------------------
+
+#include "stdafx.h"
+#include "Mesh.h"
+
+//------------------------------------------------------------------------------
+// Private Constants:
+//------------------------------------------------------------------------------
+
+//------------------------------------------------------------------------------
+// Private Structures:
+//------------------------------------------------------------------------------
+
+//------------------------------------------------------------------------------
+// Public Variables:
+//------------------------------------------------------------------------------
+
+//------------------------------------------------------------------------------
+// Private Variables:
+//------------------------------------------------------------------------------
+
+//------------------------------------------------------------------------------
+// Private Function Declarations:
+//------------------------------------------------------------------------------
+
+//------------------------------------------------------------------------------
+// Public Functions:
+//------------------------------------------------------------------------------
+// Dynamically allocate a new Mesh object but leave it empty.
+// (Hint: Use calloc() to ensure that all member variables are initialized to 0.)
+// Returns:
+//	 If the mesh was created successfully,
+//	   then return a pointer to the created Mesh,
+//	   else return NULL.
+Mesh* MeshCreate()
+{
+	return NULL;
+}
+
+// Build a quadrilateral mesh and store it in the specified Mesh object.
+// (NOTE: The DGL_Mesh object must be created using DGL_Graphics_StartMesh,
+//    DGL_Graphics_AddTriangle, and DGL_Graphics_EndMesh.)
+// (NOTE: The Mesh name can be stored using strcpy_s(). For example:
+//    strcpy_s(mesh->name, _countof(mesh->name), name); )
+// (NOTE: The drawMode should be set to DGL_DM_TRIANGLELIST.)
+// Params:
+//   mesh = Pointer to an existing, empty Mesh object.
+//	 xHalfSize = The X half-size of the mesh.
+//	 yHalfSize = The Y half-size of the mesh.
+//   uSize = The U size of the mesh, relative to texture coordinates (0.0 .. 1.0).
+//   vSize = The V size of the mesh, relative to texture coordinates (0.0 .. 1.0).
+//	 name = A name for the mesh.
+void MeshBuildQuad(Mesh* mesh, float xHalfSize, float yHalfSize, float uSize, float vSize, const char* name)
+{
+	UNREFERENCED_PARAMETER(mesh);
+	UNREFERENCED_PARAMETER(name);
+	UNREFERENCED_PARAMETER(uSize);
+	UNREFERENCED_PARAMETER(vSize);
+	UNREFERENCED_PARAMETER(xHalfSize);
+	UNREFERENCED_PARAMETER(yHalfSize);
+}
+
+// Build a "spaceship" mesh and store it in the specified Mesh object.
+// (NOTE: This must be a "unit"-sized triangular mesh with the same characteristics as
+//    the "triangular, colored mesh" created in DemoScene.c.)
+// (NOTE: The DGL_Mesh object must be created using DGL_Graphics_StartMesh,
+//    DGL_Graphics_AddTriangle, and DGL_Graphics_EndMesh.)
+// (NOTE: The Mesh name can be stored using strcpy_s().)
+// (NOTE: The drawMode should be set to DGL_DM_TRIANGLELIST.)
+// Params:
+//   mesh = Pointer to an existing, empty Mesh object.
+void MeshBuildSpaceship(Mesh* mesh)
+{
+	UNREFERENCED_PARAMETER(mesh);
+}
+
+// Render a mesh.
+// (NOTE: This is done using DGL_Graphics_DrawMesh().)
+// Params:
+//   mesh = Pointer to a Mesh to be rendered.
+void MeshRender(const Mesh* mesh)
+{
+	UNREFERENCED_PARAMETER(mesh);
+}
+
+// Free the memory associated with a mesh.
+// (NOTE: The DGL_Mesh resource must be freed using DGL_Graphics_FreeMesh().)
+// (NOTE: The Mesh object must be freed using free().
+// (NOTE: The Mesh pointer must be set to NULL.)
+// Params:
+//   mesh = Pointer to the Mesh pointer.
+void MeshFree(Mesh** mesh)
+{
+	UNREFERENCED_PARAMETER(mesh);
+}
+
+//------------------------------------------------------------------------------
+// Private Functions:
+//------------------------------------------------------------------------------
+
