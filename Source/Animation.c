01d7f4683fc89aac30ef2426a44df331fa96a89e
Mako Joseph Bryant <mako.j.bryant@outlook.com>
2025-02-13 16:08:15 -0800

    📁 create new .c modules and add stub functions
    - CheatSystem.c
    - Animation.c
    - Matrix2D.c

---
+++ Source/Animation.c
@@ -0,0 +1,148 @@
+//------------------------------------------------------------------------------
+//
+// File Name:	Animation.c
+// Author(s):	Mako J. Bryant (0068410)
+// Project:		Project 3
+// Course:		CS230S25
+//
+// Copyright © 2025 DigiPen (USA) Corporation.
+//
+//------------------------------------------------------------------------------
+
+#include "stdafx.h"
+#include "Animation.h"
+
+//------------------------------------------------------------------------------
+// Private Constants:
+//------------------------------------------------------------------------------
+
+//------------------------------------------------------------------------------
+// Private Structures:
+//------------------------------------------------------------------------------
+typedef struct Animation
+{
+	// Pointer to the parent Entity associated with the Animation component.
+	Entity* parent;
+
+	// The current frame being displayed.
+	unsigned int frameIndex;
+
+	// The maximum number of frames in the sequence.
+	unsigned int frameCount;
+
+	// The time remaining for the current frame.
+	float frameDelay;
+
+	// The amount of time to display each successive frame.
+	float frameDuration;
+
+	// True if the animation is running; false if the animation has stopped.
+	bool isRunning;
+
+	// True if the animation loops back to the beginning.
+	bool isLooping;
+
+	// True if the end of the animation sequence has been reached, false otherwise.
+	// (Hint: This should be true for only one game loop.)
+	bool isDone;
+
+} Animation;
+
+//------------------------------------------------------------------------------
+// Public Variables:
+//------------------------------------------------------------------------------
+
+//------------------------------------------------------------------------------
+// Private Variables:
+//------------------------------------------------------------------------------
+
+//------------------------------------------------------------------------------
+// Private Function Declarations:
+//------------------------------------------------------------------------------
+
+//------------------------------------------------------------------------------
+// Public Functions:
+//------------------------------------------------------------------------------
+
+
+// Dynamically allocate a new Animation component.
+// (Hint: Use calloc() to ensure that all member variables are initialized to 0.)
+Animation* AnimationCreate(void)
+{
+	return NULL;
+}
+
+// Free the memory associated with an Animation component.
+// (NOTE: The Animation pointer must be set to NULL.)
+// Params:
+//	 animation = Pointer to the Animation pointer.
+void AnimationFree(Animation** animation)
+{
+	UNREFERENCED_PARAMETER(animation);
+}
+
+// Read the properties of an Animation component from a file.
+// [NOTE: Read the integer values for frameIndex and frameCount.]
+// [NOTE: Read the float values for frameDelay and frameDuration.]
+// [NOTE: Read the boolean values for isRunning and isLooping.]
+// Params:
+//	 animation = Pointer to the Animation component.
+//	 stream = The data stream used for reading.
+void AnimationRead(Animation* animation, Stream stream)
+{
+	UNREFERENCED_PARAMETER(animation);
+	UNREFERENCED_PARAMETER(stream);
+}
+
+// Set the parent Entity for an Animation component.
+// Params:
+//	 animation = Pointer to the Animation component.
+//	 parent = Pointer to the parent Entity.
+void AnimationSetParent(Animation* animation, Entity* parent)
+{
+	UNREFERENCED_PARAMETER(animation);
+	UNREFERENCED_PARAMETER(parent);
+}
+
+// Play a simple animation sequence [0 .. frameCount - 1].
+// (Hint: This function must initialize all variables, except for "parent".)
+// Params:
+//	 animation = Pointer to the Animation component.
+//	 frameCount = The number of frames in the sequence.
+//	 frameDuration = The amount of time to display each frame (in seconds).
+//	 isLooping = True if the animation loops, false otherwise.
+void AnimationPlay(Animation* animation, int frameCount, float frameDuration, bool isLooping)
+{
+	UNREFERENCED_PARAMETER(animation);
+	UNREFERENCED_PARAMETER(frameCount);
+	UNREFERENCED_PARAMETER(frameDuration);
+	UNREFERENCED_PARAMETER(isLooping);
+}
+
+// Update the animation.
+// Params:
+//	 animation = Pointer to the Animation component.
+//	 dt = Change in time (in seconds) since the last game loop.
+void AnimationUpdate(Animation* animation, float dt)
+{
+	UNREFERENCED_PARAMETER(animation);
+	UNREFERENCED_PARAMETER(dt);
+}
+
+// Determine if the animation has reached the end of its sequence.
+// Params:
+//	 animation = Pointer to the Animation component.
+// Returns:
+//	 If the Animation pointer is valid,
+//		then return the value in isDone,
+//		else return false.
+bool AnimationIsDone(const Animation* animation)
+{
+	UNREFERENCED_PARAMETER(animation);
+	return 0;
+}
+
+//------------------------------------------------------------------------------
+// Private Functions:
+//------------------------------------------------------------------------------
+
---
+++ Source/Matrix2D.c
@@ -0,0 +1,117 @@
+//------------------------------------------------------------------------------
+//
+// File Name:	Matrix2D.c
+// Author(s):	Mako J. Bryant (0068410)
+// Project:		Project 3
+// Course:		CS230S25
+//
+// Copyright © 2025 DigiPen (USA) Corporation.
+//
+//------------------------------------------------------------------------------
+
+#include "stdafx.h"
+#include "Matrix2D.h"
+
+//------------------------------------------------------------------------------
+// Private Constants:
+//------------------------------------------------------------------------------
+
+//------------------------------------------------------------------------------
+// Private Structures:
+//------------------------------------------------------------------------------
+
+//------------------------------------------------------------------------------
+// Public Variables:
+//------------------------------------------------------------------------------
+
+//------------------------------------------------------------------------------
+// Private Variables:
+//------------------------------------------------------------------------------
+
+//------------------------------------------------------------------------------
+// Private Function Declarations:
+//------------------------------------------------------------------------------
+
+//------------------------------------------------------------------------------
+// Public Functions:
+//------------------------------------------------------------------------------
+
+
+// This function sets the matrix equal to the identity matrix.
+void Matrix2DIdentity(Matrix2D* pResult)
+{
+	UNREFERENCED_PARAMETER(pResult);
+}
+
+// This function calculates the transpose matrix of Mtx and saves it in the result matrix.
+// (NOTE: Care must be taken when pResult = pMtx.)
+void Matrix2DTranspose(Matrix2D* pResult, const Matrix2D* pMtx)
+{
+	UNREFERENCED_PARAMETER(pResult);
+	UNREFERENCED_PARAMETER(pMtx);
+}
+
+// This function multiplies Mtx0 with Mtx1 and saves the result in Result.
+// Result = Mtx0*Mtx1
+// (NOTE: Care must be taken when pResult = either pMtx0 or pMtx1.)
+void Matrix2DConcat(Matrix2D* pResult, const Matrix2D* pMtx0, const Matrix2D* pMtx1)
+{
+	UNREFERENCED_PARAMETER(pResult);
+	UNREFERENCED_PARAMETER(pMtx0);
+	UNREFERENCED_PARAMETER(pMtx1);
+}
+
+// This function creates a translation matrix from x & y and saves it in Result.
+// (Hint: This function must initialize all matrix values.)
+void Matrix2DTranslate(Matrix2D* pResult, float x, float y)
+{
+	UNREFERENCED_PARAMETER(pResult);
+	UNREFERENCED_PARAMETER(x);
+	UNREFERENCED_PARAMETER(y);
+}
+
+// This function creates a scaling matrix from x & y and saves it in Result.
+// (Hint: This function must initialize all matrix values.)
+void Matrix2DScale(Matrix2D* pResult, float x, float y)
+{
+	UNREFERENCED_PARAMETER(pResult);
+	UNREFERENCED_PARAMETER(x);
+	UNREFERENCED_PARAMETER(y);
+}
+
+// This matrix creates a rotation matrix from "Angle" whose value is in degrees.
+// (Hint: This function must initialize all matrix values.)
+// Converting from degrees to radians can be performed as follows:
+//	 radians = (angle * M_PI) / 180.0f
+// M_PI is defined in "math.h", which may be included as follows:
+//   #define _USE_MATH_DEFINES
+//   #include <math.h>
+void Matrix2DRotDeg(Matrix2D* pResult, float angle)
+{
+	UNREFERENCED_PARAMETER(pResult);
+	UNREFERENCED_PARAMETER(angle);
+}
+
+// This matrix creates a rotation matrix from "Angle" whose value is in radians.
+// (Hint: This function must initialize all matrix values.)
+void Matrix2DRotRad(Matrix2D* pResult, float angle)
+{
+	UNREFERENCED_PARAMETER(pResult);
+	UNREFERENCED_PARAMETER(angle);
+}
+
+// This function multiplies the matrix Mtx with the vector Vec and saves the result in Result.
+// Result = Mtx * Vec.
+// (NOTE: Care must be taken when pResult = pVec.)
+// (NOTE: Remember to treat the implied w component as 1.)
+void Matrix2DMultVec(Vector2D* pResult, const Matrix2D* pMtx, const Vector2D* pVec)
+{
+	UNREFERENCED_PARAMETER(pResult);
+	UNREFERENCED_PARAMETER(pMtx);
+	UNREFERENCED_PARAMETER(pVec);
+}
+
+//------------------------------------------------------------------------------
+// Private Functions:
+//------------------------------------------------------------------------------
+
